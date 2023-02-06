8abc4ec5b74862c760cda3a415bc46ad1ca5c499
right101 <raya.mogilev@gmail.com>
2023-02-06 14:44:28 -0600

    updated security group

--- session-2/tasks/security-group/main.tf
+++ session-2/tasks/security-group/main.tf
@@ -5,8 +5,35 @@ resource "aws_security_group" "first_sg" {
 
     ingress {
     description      = "TLS from VPC"
-    from_port        = 80, 443, 22, 3306
-    to_port          = 80, 443, 22, 3306
+    from_port        = 80 
+    to_port          = 80 
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    
+  }
+  
+  ingress {
+    description      = "TLS from VPC"
+    from_port        = 443 
+    to_port          = 443 
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    
+  }
+
+ingress {
+    description      = "TLS from VPC"
+    from_port        = 22 
+    to_port          = 22 
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    
+  }
+
+  ingress {
+    description      = "TLS from VPC"
+    from_port        = 3306 
+    to_port          = 3306
     protocol         = "tcp"
     cidr_blocks      = ["0.0.0.0/0"]
     
