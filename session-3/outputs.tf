648e8b37414cf01bdd1b80141c6891606d09da9e
right101 <raya.mogilev@gmail.com>
2023-02-06 21:47:56 -0600

    updated output

--- session-3/outputs.tf
+++ session-3/outputs.tf
@@ -1,11 +1,11 @@
 # A root module can use outputs to print certain values in the CLI output after running terraform apply
 
 output "instance_ip_address" {
-    value       = aws_instance.main.public_ip
+    value       = aws_instance.first_ec2.public_ip
     description = "The public IP address of main EC2 instance" 
 }
 
 output "instance_state" {
-    value       = aws_instance.main.instance_state
+    value       = aws_instance.first_ec2.instance_state
     description = "The instance state of the main EC2 instance" 
 }
