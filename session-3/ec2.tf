5a58dab1a766f5c40c18d3117ca0d9e965fc1d09
right101 <raya.mogilev@gmail.com>
2023-02-06 20:35:30 -0600

    tags added

--- session-3/ec2.tf
+++ session-3/ec2.tf
@@ -1,5 +1,8 @@
 resource "aws_instance" "first_ec2" {
   ami           = "ami-0aa7d40eeae50c9a9"
   instance_type = "t2.micro"
-  vpc_security_group_ids = [aws_security_group.main.id]
+  vpc_security_group_ids = [aws_security_group.main.id] # a list of strings # dynamic reference
+  tags = {  # a map
+     Name = "development"
+  }
 }
