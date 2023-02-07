f93ce20d5e497b4dd37b08a24e94db97def6c6cd
right101 <raya.mogilev@gmail.com>
2023-02-06 20:04:16 -0600

    updated sg

--- session-3/sg.tf
+++ session-3/sg.tf
@@ -1,11 +1,11 @@
-resource "aws_security_group" "main" {
+resource "aws_security_group" "main_sg" {
     name        = "main"
     description = " Allow ssh inbound traffic" # This security group opens all ports from anywhere. TLS - transport layer security
     vpc_id      = "vpc-0045501a6a1a9ed00"
 
     ingress {
     description      = "TLS from VPC"
-    from_port        = 22 
+    from_port        = 22 # single value
     to_port          = 22 
     protocol         = "tcp" #string, one thing only
     cidr_blocks      = ["0.0.0.0/0"] # list of string
