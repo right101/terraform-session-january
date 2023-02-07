4b5904506dba5dd0a075e812f8ffd5cc432409dc
right101 <raya.mogilev@gmail.com>
2023-02-07 16:19:24 -0600

    subnets updated

--- tasks/vpc/subnets.tf
+++ tasks/vpc/subnets.tf
@@ -1,39 +1,39 @@
-resource "aws_subnet" "main" {
+resource "aws_subnet" "public_subnet_1" {
   vpc_id     = [aws_vpc.main.id]  
   cidr_block = "10.0.1.0/24"
   availability_zone = "us-east-1a"
   tags = {
     Name = "Public-subnet-1"
   }
-resource "aws_subnet" "main" {
+resource "aws_subnet" "public_subnet_2" {
   vpc_id     = [aws_vpc.main.id]  
   cidr_block = "10.0.2.0/24"
   availability_zone = "us-east-1b"
   tags = {
     Name = "Public-subnet-2"
   }
-resource "aws_subnet" "main" {
+resource "aws_subnet" "public_subnet_3" {
   vpc_id     = [aws_vpc.main.id]  
   cidr_block = "10.0.3.0/24"
   availability_zone = "us-east-1c"
   tags = {
     Name = "Public-subnet-3"
   }
-resource "aws_subnet" "main" {
+resource "aws_subnet" "private_subnet_1" {
   vpc_id     = [aws_vpc.main.id]  
   cidr_block = "10.0.11.0/24"
   availability_zone = "us-east-1a"
   tags = {
     Name = "Private-subnet-1"
   }
-resource "aws_subnet" "main" { 
+resource "aws_subnet" "private_subnet_2" { 
   vpc_id     = [aws_vpc.main.id]  
   cidr_block = "10.0.12.0/24"
   availability_zone = "us-east-1b"
   tags = {
     Name = "Private-subnet-2"
   }
-resource "aws_subnet" "main" {
+resource "aws_subnet" "private_subnet_3" {
   vpc_id     = [aws_vpc.main.id]  
   cidr_block = "10.0.13.0/24"
   availability_zone = "us-east-1c"
