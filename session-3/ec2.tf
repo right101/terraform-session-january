1317158cccd9ddef5324359fca447ef19a59ebb4
right101 <raya.mogilev@gmail.com>
2023-02-06 20:15:49 -0600

    updated security-group

--- session-3/ec2.tf
+++ session-3/ec2.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "first_ec2" {
   ami           = "ami-0aa7d40eeae50c9a9"
   instance_type = "t2.micro"
-  vpc_security_group_ids = ["aws_security_group.main_sg.id"]
+  vpc_security_group_ids = [aws_security_group.main.id]
 }
--- session-3/sg.tf
+++ session-3/sg.tf
@@ -1,4 +1,4 @@
-resource "aws_security_group" "main_sg" {
+resource "aws_security_group" "main" {
     name        = "main"
     description = " Allow ssh inbound traffic" # This security group opens all ports from anywhere. TLS - transport layer security
     vpc_id      = "vpc-0045501a6a1a9ed00"
