59458c7ad6cef617faa24c7f504cb7b20bb57f43
right101 <raya.mogilev@gmail.com>
2023-02-07 16:12:24 -0600

    subnets added

---
+++ tasks/vpc/subnets.tf
@@ -0,0 +1,50 @@
+resource "aws_subnet" "main" {
+  vpc_id     = [aws_vpc.main.id]  
+  cidr_block = "10.0.1.0/24"
+  availability_zone = "us-east-1a"
+  tags = {
+    Name = "Public-subnet-1"
+  }
+resource "aws_subnet" "main" {
+  vpc_id     = [aws_vpc.main.id]  
+  cidr_block = "10.0.2.0/24"
+  availability_zone = "us-east-1b"
+  tags = {
+    Name = "Public-subnet-2"
+  }
+resource "aws_subnet" "main" {
+  vpc_id     = [aws_vpc.main.id]  
+  cidr_block = "10.0.3.0/24"
+  availability_zone = "us-east-1c"
+  tags = {
+    Name = "Public-subnet-3"
+  }
+resource "aws_subnet" "main" {
+  vpc_id     = [aws_vpc.main.id]  
+  cidr_block = "10.0.11.0/24"
+  availability_zone = "us-east-1a"
+  tags = {
+    Name = "Private-subnet-1"
+  }
+resource "aws_subnet" "main" { 
+  vpc_id     = [aws_vpc.main.id]  
+  cidr_block = "10.0.12.0/24"
+  availability_zone = "us-east-1b"
+  tags = {
+    Name = "Private-subnet-2"
+  }
+resource "aws_subnet" "main" {
+  vpc_id     = [aws_vpc.main.id]  
+  cidr_block = "10.0.13.0/24"
+  availability_zone = "us-east-1c"
+  tags = {
+    Name = "Private-subnet-3"
+} 
+ }
+  }  
+   }  
+    }
+     }
+      }
+
+      #Here I created 3 privite subnets and 3 public subnets assoiated with VPC
