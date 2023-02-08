97341d08e0aa3111f3ee9cffb46c0415ebac2120
right101 <raya.mogilev@gmail.com>
2023-02-08 17:01:22 -0600

    nat gateway added

--- tasks/vpc/ngw.tf
+++ tasks/vpc/ngw.tf
@@ -1,6 +1,6 @@
 resource "aws_nat_gateway" "main" {
-  allocation_id = aws_eip.elastic.id
-  connectivity_type = "private"
+  allocation_id = aws_eip_association.elastic.id
+  connectivity_type = "public"
   subnet_id         = aws_subnet.public_subnet_1.id
   tags = {
     Name = "main"
