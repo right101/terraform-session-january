87d21af0b9bc9864ad2b873399b1b3bdf363f6f1
right101 <raya.mogilev@gmail.com>
2023-02-08 16:12:37 -0600

    routes updated

--- tasks/vpc/route_tables.tf
+++ tasks/vpc/route_tables.tf
@@ -1,6 +1,6 @@
 resource "aws_route_table" "public" {
     vpc_id     = aws_vpc.main.id 
-    route = {
+    route  {
         cidr_block = "0.0.0.0/0"
         gateway_id = aws_internet_gateway.main.id
     }
