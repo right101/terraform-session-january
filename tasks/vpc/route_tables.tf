854af44d22c53c4cfca6949c31a9621a6a338d1d
right101 <raya.mogilev@gmail.com>
2023-02-08 16:09:03 -0600

    updated routes

--- tasks/vpc/route_tables.tf
+++ tasks/vpc/route_tables.tf
@@ -3,26 +3,8 @@ resource "aws_route_table" "public" {
     route = {
         cidr_block = "0.0.0.0/0"
         gateway_id = aws_internet_gateway.main.id
-        subnet_id = aws_subnet.public_subnet_1.id
     }
-}
-resource "aws_route_table" "public" {
-    vpc_id     = aws_vpc.main.id 
-    route = {
-        cidr_block = "0.0.0.0/0"
-        gateway_id = aws_internet_gateway.main.id
-        subnet_id = aws_subnet.public_subnet_2.id
-    }
-}
-resource "aws_route_table" "public" {
-    vpc_id     = aws_vpc.main.id 
-    route = {
-        cidr_block = "0.0.0.0/0"
-        gateway_id = aws_internet_gateway.main.id
-        subnet_id = aws_subnet.public_subnet_3.id
-    }
-}
     tags = {
         Name = "Public_rt"
     }
-
+}
