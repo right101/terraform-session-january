06f77d8feac3743459d14aea6aafd867fa21b22f
right101 <raya.mogilev@gmail.com>
2023-02-08 17:13:42 -0600

    updated private association

--- tasks/vpc/private_association.tf
+++ tasks/vpc/private_association.tf
@@ -1,13 +1,13 @@
-resource "aws_route_table_association" "a" {
+resource "aws_route_table_association" "d" {
   subnet_id      = aws_subnet.private_subnet_1.id
   route_table_id = aws_route_table.private.id
 }
 
-resource "aws_route_table_association" "b" {
+resource "aws_route_table_association" "e" {
   subnet_id      = aws_subnet.private_subnet_2.id
   route_table_id = aws_route_table.private.id
 }
-resource "aws_route_table_association" "c" {
+resource "aws_route_table_association" "f" {
   subnet_id      = aws_subnet.private_subnet_3.id
   route_table_id = aws_route_table.private.id
 }
