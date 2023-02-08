1390428f8c5102f13968d66aaab41f0fa815b908
right101 <raya.mogilev@gmail.com>
2023-02-08 15:51:35 -0600

    updated route table

--- tasks/vpc/route_tables.tf
+++ tasks/vpc/route_tables.tf
@@ -2,11 +2,27 @@ resource "aws_route_table" "public" {
     vpc_id     = aws_vpc.main.id 
     route = {
         cidr_block = "0.0.0.0/0"
-        gateway_id = aws_internet_gateway.gw.id
+        gateway_id = aws_internet_gateway.main.id
+        subnet_id = aws_subnet.public_subnet_1.id
     }
-   subnet_id =[aws_subnet.public_subnet_1.id, aws_subnet.public_subnet_2.id, aws_subnet.public_subnet_3.id]
+}
+resource "aws_route_table" "public" {
+    vpc_id     = aws_vpc.main.id 
+    route = {
+        cidr_block = "0.0.0.0/0"
+        gateway_id = aws_internet_gateway.main.id
+        subnet_id = aws_subnet.public_subnet_2.id
+    }
+}
+resource "aws_route_table" "public" {
+    vpc_id     = aws_vpc.main.id 
+    route = {
+        cidr_block = "0.0.0.0/0"
+        gateway_id = aws_internet_gateway.main.id
+        subnet_id = aws_subnet.public_subnet_3.id
+    }
+}
     tags = {
         Name = "Public_rt"
     }
-}
 
