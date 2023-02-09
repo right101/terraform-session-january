34bc1075690152e259960fc82f934ea4f913dc60
right101 <raya.mogilev@gmail.com>
2023-02-09 14:01:33 -0600

    security group v1.1 added

---
+++ tasks/security-groupv1.1/main.tf
@@ -0,0 +1,49 @@
+resource "aws_security_group" "task_sg" {
+    name        = var.name
+    description = "Allow inbound traffic" 
+    vpc_id      = var.vpc_id
+
+    ingress {
+    description      = "this open port ${var.port_a} "
+    from_port        = var.port_a 
+    to_port          = var.port_a
+    protocol         = var.protocol
+    cidr_blocks      = var.cidr_blocks
+    
+  }
+
+  ingress {
+    description      = "this open port ${var.port_b}"
+    from_port        = var.port_b
+    to_port          = var.port_b
+    protocol         = var.protocol
+    cidr_blocks      = var.cidr_blocks
+    
+  }
+
+ingress {
+    description      = "this open port ${var.port_c}"
+    from_port        = var.port_c
+    to_port          = var.port_c
+    protocol         = var.protocol
+    cidr_blocks      = var.cidr_blocks
+    
+  }
+
+  ingress {
+    description      = "this open port ${var.port_d}"
+    from_port        = var.port_d
+    to_port          = var.port_d
+    protocol         = var.protocol
+    cidr_blocks      = var.cidr_blocks
+    
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = var.cidr_blocks
+   
+  }
+}
---
+++ tasks/security-groupv1.1/variables.tf
@@ -0,0 +1,41 @@
+variable "name" {
+    type = string
+    description = "This is name for security group"
+    default  = "Task_sg"
+}
+
+variable "vpc_id" {
+    type = string
+    description = "this is for vpsc"
+    default  = "vpc-0045501a6a1a9ed00"
+}
+variable "port_a" {
+    type = string
+    description = "this is for ssh"
+    default  = "22"
+}
+variable "port_b" {
+    type = string
+    description = "this is for http"
+    default  = "80"
+}
+variable "port_c" {
+    type = string
+    description = "this to open port 3306"
+    default  = "3306"
+}
+variable "port_d" {
+    type = string
+    description = "this is for https"
+    default  = "443"
+}
+variable "cidr_blocks" {
+    type = string
+    description = "this is for cidr block"
+    default  = "0.0.0.0/0"
+}
+variable "protocol" {
+    type = string
+    description = "this is for protocol"
+    default  = "tcp"
+}
