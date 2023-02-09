da4bb22310a107ccf4ae9b77c6d46ecd06998ee8
right101 <raya.mogilev@gmail.com>
2023-02-08 21:29:47 -0600

    updated name_tag

--- session-4/ec2.tf
+++ session-4/ec2.tf
@@ -4,7 +4,8 @@ resource "aws_instance" "first_ec2" {
   vpc_security_group_ids = [aws_security_group.main.id] # a list of strings # dynamic reference
   tags = {  # a map
      Name = "${var.env}-instance" # half is variable and half is hardcoded and it is in double quotation
-  }
+     Name1 = format("%s-instance", var.env) # dev-instance
+    }
 }
 resource "aws_instance" "first_ec2_frontend" {
   ami           = data.aws_ami.amazon_linux_2.id
@@ -12,5 +13,6 @@ resource "aws_instance" "first_ec2_frontend" {
   vpc_security_group_ids = [aws_security_group.main.id] # a list of strings # dynamic reference
   tags = {  # a map
      Name = "${var.env}-frontend-instance" # 
+     Name1 = format("%s-frontend-instance", var.env) # dev-instance
   }
 }
