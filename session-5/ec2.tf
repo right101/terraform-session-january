a82370292f27b038aff3fbec290afaa4cdb9f329
right101 <raya.mogilev@gmail.com>
2023-02-11 12:25:46 -0600

    count added

--- session-5/ec2.tf
+++ session-5/ec2.tf
@@ -1,4 +1,5 @@
 resource "aws_instance" "first_ec2" {
+  count = 3
   ami           = data.aws_ami.amazon_linux_2.id
   instance_type = var.instance_type
   vpc_security_group_ids = [aws_security_group.main.id] # a list of strings # dynamic reference
@@ -7,3 +8,4 @@ resource "aws_instance" "first_ec2" {
      
 }
 }
+# count is meta argument 
