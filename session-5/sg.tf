b93b90d7462097ca10efdaaf0b5fcd78ecf6a439
right101 <raya.mogilev@gmail.com>
2023-02-11 12:44:50 -0600

    sg name added

--- session-5/sg.tf
+++ session-5/sg.tf
@@ -2,6 +2,9 @@ resource "aws_security_group" "main" {
     name        = "main"
     description = " Allow ssh inbound traffic" # This security group opens all ports from anywhere. TLS - transport layer security
     vpc_id      = "vpc-0045501a6a1a9ed00"
+    tags = {
+    Name = "main_sg"
+  }
 }
 resource "aws_security_group_rule" "ingress" {
   type              = "ingress"
