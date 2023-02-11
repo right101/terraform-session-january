63aa78bf2af6bdf5e56ba7ecdeddaf2cbe659a69
right101 <raya.mogilev@gmail.com>
2023-02-11 12:24:52 -0600

    count added

--- session-5/backend.tf
+++ session-5/backend.tf
@@ -3,7 +3,8 @@ terraform {
       bucket = "terraform-backend-raya"
       region = "us-east-1"
       key = "session/terraform.tfstate" # where does terraform need to store your file. Path to to Prefix
-      dynamodb_table = "terraform-session-sep-state-lock" # Lock Table name. When setting up we need to LockID capital
+      dynamodb_table = "terraform-session-sep-state-lock" # Lock Table name. When setting up we need to LockID capital. 
+                                                          #This is to prevent paralel execution
     }
 }
 # 1.you cannot useor pass any variables in key. Key most likely hard coded
