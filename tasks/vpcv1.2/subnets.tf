f72c501d4c091f31f397b48c2316b4f187fbf26d
right101 <raya.mogilev@gmail.com>
2023-02-13 14:48:16 -0600

    count changed

--- tasks/vpcv1.2/subnets.tf
+++ tasks/vpcv1.2/subnets.tf
@@ -1,6 +1,6 @@
 
 resource "aws_subnet" "public_subnet" {
-  count = length(var.availability_zone )
+  count = length(var.public_subnet_cidr_blocks )
   vpc_id     = aws_vpc.main.id  
   cidr_block = element(var.public_subnet_cidr_blocks, count.index)
   availability_zone = element(var.availability_zones, count.index)
@@ -12,7 +12,7 @@ resource "aws_subnet" "public_subnet" {
 
 
 resource "aws_subnet" "private_subnet" {
-  count = length(var.availability_zone )
+  count = length(var.private_subnet_cidr_blocks )
   vpc_id     = aws_vpc.main.id  
   cidr_block = element(var.private_subnet_cidr_blocks, count.index)
   availability_zone = element(var.availability_zones, count.index)
