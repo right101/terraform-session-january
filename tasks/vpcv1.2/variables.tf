39a7c14fc44304a720b922855aee5badcebacc66
right101 <raya.mogilev@gmail.com>
2023-02-13 00:56:02 -0600

    updated vpc_v1.2

--- tasks/vpcv1.2/variables.tf
+++ tasks/vpcv1.2/variables.tf
@@ -59,12 +59,12 @@ variable "public_subnet_cidr_blocks" {
     default = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
 }
 variable "private_subnet_cidr_blocks" {
-    type = string
+    type = list(string)
     description = "List of cidr blocks"
     default  = ["10.0.11.0/24", "10.0.12.0/24", "10.0.13.0/24"]
 }
 variable "availability_zones" {
-    type = string
+    type = list(string)
     description = "this is for AZs"
     default  = ["us-east-1a", "us-east-1b", "us-east-1c"]                
 }                                          
