1bcddaa9200a43b1e9fb913aa4de297e98788c41
right101 <raya.mogilev@gmail.com>
2023-02-20 20:20:15 -0600

    null resource updated

--- session-8/main.tf
+++ session-8/main.tf
@@ -38,7 +38,7 @@ output "instance_ip" {
     value = aws_instance.main.public_ip
 }
 
-resource "nul_resource" "local_script" {
+resource "null_resource" "local_script" {
    provisioner "local-exec" {
      command = "echo 'Hello from the Local Server' >> local.txt"
    }  
