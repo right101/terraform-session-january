5350e577b17e275f0bc88c1a3c52b608cc7020a7
right101 <raya.mogilev@gmail.com>
2023-02-20 18:53:04 -0600

    backend changed

--- session-8/backend copy.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-    backend "s3" {
-      bucket = "terraform-backend-raya"
-      region = "us-east-1"
-      key = "session-7/frontend/terraform.tfstate" # where does terraform need to store your file. Path to to Prefix
-      dynamodb_table = "terraform-session-sep-state-lock" # Lock Table name. When setting up we need to LockID capital. 
-
-    }
-}
