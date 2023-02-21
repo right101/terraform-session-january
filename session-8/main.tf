b7399da3fc0e97c2476830b04239a79a02d1e7c8
right101 <raya.mogilev@gmail.com>
2023-02-20 19:12:30 -0600

    file added

--- session-8/main.tf
+++ session-8/main.tf
@@ -3,6 +3,18 @@ resource "aws_instance" "main" {
     instance_type = "t2.micro"
     vpc_security_group_ids = [ aws_security_group.main.id ]
     key_name = aws_key_pair.terraform_server.key_name
+
+    provisioner "file" {
+        source = "~/terraform-session-january/session-8/index.html" #path on Terraform Server
+        destination = "/tmp/index.html" # path for the remote server
+
+        connection {
+            type = "ssh"
+            user = "ec2-user"
+            host = self.public.ip
+            private_key = file("~/.ssh/id_rsa")
+        }
+    }
 }
 resource "aws_key_pair" "terraform_server" {
     key_name = "Terraform-Server-Key"
--- session-8/sg.tf
+++ session-8/sg.tf
@@ -2,29 +2,20 @@ resource "aws_security_group" "main" {
     name        = "main"
     description = " Allow ssh inbound traffic" # This security group opens all ports from anywhere. TLS - transport layer security
     vpc_id      = "vpc-0045501a6a1a9ed00"
-    tags = {
-      Name = "main_sg"
+
+    ingress {
+    description      = "TLS from VPC"
+    from_port        = 22 # single value
+    to_port          = 22 
+    protocol         = "tcp" #string, one thing only
+    cidr_blocks      = ["0.0.0.0/0"] # list of string
+    
+  }
+   egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+   
   }
 }
-resource "aws_security_group_rule" "ingress" {
-  count = length(var.ports) # it will count legth of ports in the list in variables.tf
-  type              = "ingress"
-  to_port           = element( var.ports, count.index) #22,80,3306
-  protocol          = "tcp"
-  from_port         = element( var.ports, count.index) #22,80,3306
-  security_group_id = aws_security_group.main.id
-  cidr_blocks       = ["0.0.0.0/0"]
-}
-resource "aws_security_group_rule" "egress" {
-  type              = "egress"
-  to_port           = 0
-  protocol          = "-1"
-  from_port         = 0
-  security_group_id = aws_security_group.main.id
-  cidr_blocks       = ["0.0.0.0/0"]
-}
-# count = 2 
-# count.index = 0,1
-
-#count = 5
-#count = 0,1,2,3,4,
