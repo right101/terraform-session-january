182fcba7723474721d25afc67f07b4711e699cb3
right101 <raya.mogilev@gmail.com>
2023-02-22 20:46:49 -0600

    module updated

--- session-9/dev/main.tf
+++ session-9/dev/main.tf
@@ -8,10 +8,12 @@ module "ec2_instance" {
   env = "dev"
   ami = "ami-0aa7d40eeae50c9a9"
   instance_type = "t2.micro"
-  sg = [module.ec2_sg.id]  ## module.module_name.attribute
+  sg = [module.ec2_sg.main_sg_id]  ## module.module_name.attribute
 }
 module "ec2_sg" {
   source = "../../modules/sg"
   env = "dev"
 }
 #you always reference to child module outputs
+
+#module.module_name.Output_Name
